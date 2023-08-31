0f22a15042d9e21cd94420d481879e62e4a605ab
riazufila <riazufila@gmail.com>
2023-08-31 14:02:43 +0800

    Add: Spawn location initializer.

---
+++ src/main/java/io/github/riazufila/minedoplugin/spawnlocationinitializer/SpawnLocationInitializer.java
@@ -0,0 +1,35 @@
+package io.github.riazufila.minedoplugin.spawnlocationinitializer;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import io.github.riazufila.minedoplugin.constants.spawnlocation.SpawnLocation;
+
+public class SpawnLocationInitializer {
+    private final World world;
+    public SpawnLocationInitializer(World world) {
+        this.world = world;
+
+        if (!hasSpawnLocationSet()) {
+            setSpawnLocation();
+        }
+    }
+
+    public void setSpawnLocation() {
+        this.world.setSpawnLocation(
+                SpawnLocation.positionX.getPosition(),
+                // Any highest block at Y that isn't air block.
+                this.world.getHighestBlockYAt(
+                        SpawnLocation.positionX.getPosition(),
+                        SpawnLocation.positionZ.getPosition()
+                ) + 1,
+                SpawnLocation.positionZ.getPosition()
+        );
+    }
+
+    public boolean hasSpawnLocationSet() {
+        Location spawnlocation = this.world.getSpawnLocation();
+
+        return spawnlocation.getBlockX() == SpawnLocation.positionX.getPosition()
+                && spawnlocation.getBlockZ() == SpawnLocation.positionZ.getPosition();
+    }
+}
