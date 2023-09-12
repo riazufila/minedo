ade5c3c914686e3c1b4e206b7770afe72ed17817
riazufila <riazufila@gmail.com>
2023-09-12 10:21:20 +0800

    Add: Filter commands.

---
+++ src/main/java/net/minedo/mc/customcommand/CustomCommand.java
@@ -0,0 +1,61 @@
+package net.minedo.mc.customcommand;
+
+import net.minedo.mc.Minedo;
+import net.minedo.mc.customcommand.spawn.SpawnCommand;
+import net.minedo.mc.interfaces.customcommand.CustomCommandInterface;
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerCommandSendEvent;
+
+import java.util.Objects;
+
+public class CustomCommand implements Listener {
+
+    private final Minedo pluginInstance;
+    private final Server server;
+    private final CustomCommandInterface customCommandInterface;
+
+    public CustomCommand(Minedo pluginInstance, Server server, CustomCommandInterface customCommandInterface) {
+        this.pluginInstance = pluginInstance;
+        this.server = server;
+        this.customCommandInterface = customCommandInterface;
+
+        this.setupCustomCommands();
+    }
+
+    public void setupCustomCommands() {
+        // Spawn command and listeners.
+        SpawnCommand spawnCommand = new SpawnCommand(this.pluginInstance);
+        server.getPluginManager().registerEvents(spawnCommand, this.pluginInstance);
+        Objects.requireNonNull(this.customCommandInterface.getCommand("spawn")).setExecutor(spawnCommand);
+    }
+
+    // Disable commands.
+    @EventHandler
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+        Player player = event.getPlayer();
+        String command = event.getMessage();
+
+        if (!player.isOp() && !command.substring(1).equals("spawn")) {
+            event.setCancelled(true);
+        }
+    }
+
+    // Remove commands from display.
+    @EventHandler
+    public void onPlayerCommandSend(PlayerCommandSendEvent event) {
+        Player player = event.getPlayer();
+
+        if (!player.isOp()) {
+            // Remove all commands.
+            event.getCommands().clear();
+
+            // Add custom commands to be displayed.
+            event.getCommands().add("spawn");
+        }
+    }
+
+}
