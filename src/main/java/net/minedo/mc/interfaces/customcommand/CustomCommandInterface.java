d00936e5c85bc8c4946e694e9de2c8f3e682e8bd
riazufila <riazufila@gmail.com>
2023-09-12 10:21:20 +0800

    Add: Filter commands.

---
+++ src/main/java/net/minedo/mc/interfaces/customcommand/CustomCommandInterface.java
@@ -0,0 +1,7 @@
+package net.minedo.mc.interfaces.customcommand;
+
+import org.bukkit.command.PluginCommand;
+
+public interface CustomCommandInterface {
+    PluginCommand getCommand(String string);
+}
