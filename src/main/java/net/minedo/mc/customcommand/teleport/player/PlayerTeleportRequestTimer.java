9a2d5b212453b75716307c2c05e6894d2f3a3d2f
riazufila <46749569+riazufila@users.noreply.github.com>
2023-09-14 23:03:11 +0800

    Add: Request and decline teleport.

---
+++ src/main/java/net/minedo/mc/customcommand/teleport/player/PlayerTeleportRequestTimer.java
@@ -0,0 +1,40 @@
+package net.minedo.mc.customcommand.teleport.player;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class PlayerTeleportRequestTimer extends BukkitRunnable {
+
+    private final Player requester;
+    private final Player requestee;
+    private final Map<UUID, Integer> teleportRequesters;
+    private final Map<UUID, Integer> teleportRequestees;
+
+    public PlayerTeleportRequestTimer(
+            Player requester, Player requestee,
+            Map<UUID, Integer> teleportRequesters, Map<UUID, Integer> teleportRequestees
+    ) {
+        this.requester = requester;
+        this.requestee = requestee;
+        this.teleportRequesters = teleportRequesters;
+        this.teleportRequestees = teleportRequestees;
+    }
+
+    @Override
+    public void run() {
+        teleportRequesters.remove(this.requester.getUniqueId());
+        teleportRequestees.remove(this.requestee.getUniqueId());
+
+        this.cancel();
+
+        Component timeoutMessage = Component.text("Teleport request timeout.").color(NamedTextColor.RED);
+        requester.sendMessage(timeoutMessage);
+        requestee.sendMessage(timeoutMessage);
+    }
+
+}
