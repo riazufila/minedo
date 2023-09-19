6256e8ff5b177646ade32e76a3f529954f5f6844
riazufila <46749569+riazufila@users.noreply.github.com>
2023-09-19 15:24:57 +0800

    Fix: Prevent players from being launched again based on player location and highest block Y.

--- src/main/java/net/minedo/mc/regionregeneration/RegionRegenerationScheduler.java
+++ src/main/java/net/minedo/mc/regionregeneration/RegionRegenerationScheduler.java
@@ -4,6 +4,7 @@ import com.sk89q.worldedit.WorldEdit;
 import net.minedo.mc.Minedo;
 import net.minedo.mc.models.region.Region;
 import org.bukkit.Chunk;
+import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
@@ -39,11 +40,19 @@ public class RegionRegenerationScheduler extends BukkitRunnable {
         this.restoringChunks = restoringChunks;
     }
 
+    private boolean isPlayerWithinLaunchingGround(Player player) {
+        Location location = player.getLocation();
+        int highestBlockAtY = this.world.getHighestBlockYAt(location.getBlockX(), location.getBlockZ());
+        int LAUNCHING_GROUND_MAX_HEIGHT = 5;
+
+        return location.getBlockY() - highestBlockAtY <= LAUNCHING_GROUND_MAX_HEIGHT;
+    }
+
     private void launchPlayersAbove() {
         Entity[] entities = this.chunk.getEntities();
 
         for (Entity entity : entities) {
-            if (entity instanceof Player player) {
+            if (entity instanceof Player player && this.isPlayerWithinLaunchingGround(player)) {
                 player.playSound(player.getLocation(), Sound.BLOCK_AZALEA_LEAVES_STEP, 1, 1);
                 player.setVelocity(player.getLocation().getDirection().multiply(2).setX(0).setZ(0).setY(2));
 
