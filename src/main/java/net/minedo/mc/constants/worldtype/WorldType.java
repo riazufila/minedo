9e99e3e7075ecd11fd82c5fe219471e120680ffb
riazufila <46749569+riazufila@users.noreply.github.com>
2023-09-19 19:19:57 +0800

    Update: Included all worlds to getter.

--- src/main/java/net/minedo/mc/constants/worldtype/WorldType.java
+++ src/main/java/net/minedo/mc/constants/worldtype/WorldType.java
@@ -2,7 +2,9 @@ package net.minedo.mc.constants.worldtype;
 
 public enum WorldType {
 
-    WORLD("world");
+    WORLD("world"),
+    NETHER_WORLD("world_nether"),
+    THE_END_WORLD("world_the_end");
 
     private final String type;
 
