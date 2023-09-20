0d55c8198afdcc0c8c6d2a1bf32325bbdee48fc8
riazufila <46749569+riazufila@users.noreply.github.com>
2023-09-20 12:11:57 +0800

    Update: Refactor method for Region model.

--- src/main/java/net/minedo/mc/models/region/Region.java
+++ src/main/java/net/minedo/mc/models/region/Region.java
@@ -1,5 +1,8 @@
 package net.minedo.mc.models.region;
 
+import org.bukkit.Location;
+import org.bukkit.World;
+
 public class Region {
 
     public int id;
@@ -55,6 +58,19 @@ public class Region {
 
     public void setMaxZ(int maxZ) {
         this.maxZ = maxZ;
+    }
+
+    public Location getCenterOfRegion(World world) {
+        // Add 1 to max coordinate to conform with chunk size.
+        int centerCoordinateX = (this.minX + (this.maxX + 1)) / 2;
+        int centerCoordinateZ = (this.minZ + (this.maxZ + 1)) / 2;
+
+        return new Location(
+                world,
+                centerCoordinateX,
+                world.getHighestBlockYAt(centerCoordinateX, centerCoordinateZ),
+                centerCoordinateZ
+        );
     }
 
 }
