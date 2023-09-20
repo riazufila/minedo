e15bdff3c7dd6df1489ffd0e6ddad817ea951bb2
riazufila <46749569+riazufila@users.noreply.github.com>
2023-09-20 12:14:25 +0800

    Fix: Disable tab complete for player list.

--- src/main/java/net/minedo/mc/customcommand/teleport/region/RegionTeleport.java
+++ src/main/java/net/minedo/mc/customcommand/teleport/region/RegionTeleport.java
@@ -11,6 +11,7 @@ import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,7 +20,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 
-public class RegionTeleport implements CommandExecutor, Listener {
+public class RegionTeleport implements CommandExecutor, Listener, TabCompleter {
 
     private final double destinationMinX;
     private final double destinationMaxX;
@@ -107,6 +108,13 @@ public class RegionTeleport implements CommandExecutor, Listener {
         }
 
         return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(
+            @NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args
+    ) {
+        return Collections.emptyList();
     }
 
     @EventHandler
