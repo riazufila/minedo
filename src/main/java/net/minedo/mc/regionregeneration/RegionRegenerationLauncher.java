c31e27ed43bca55388f61f34357eebdd92b30e64
riazufila <46749569+riazufila@users.noreply.github.com>
2023-09-21 08:34:14 +0800

    Update: Launch all living entities when region regenerates.

--- src/main/java/net/minedo/mc/regionregeneration/RegionRegenerationLauncher.java
+++ src/main/java/net/minedo/mc/regionregeneration/RegionRegenerationLauncher.java
@@ -6,7 +6,7 @@ import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -31,10 +31,13 @@ public class RegionRegenerationLauncher extends BukkitRunnable {
         this.pluginInstance = pluginInstance;
     }
 
-    private boolean isPlayerWithinLaunchingGround(Player player) {
-        Location location = player.getLocation();
-        int highestBlockAtY = this.region.getWorldType().getHighestBlockYAt(location.getBlockX(), location.getBlockZ());
+    private boolean isLivingEntityWithinLaunchingGround(LivingEntity livingEntity) {
+        Location location = livingEntity.getLocation();
         int LAUNCHING_GROUND_MAX_HEIGHT = 5;
+
+        int highestBlockAtY = this.region.getWorldType().getHighestBlockYAt(
+                location.getBlockX(), location.getBlockZ()
+        );
 
         return location.getBlockY() - highestBlockAtY <= LAUNCHING_GROUND_MAX_HEIGHT;
     }
@@ -43,22 +46,29 @@ public class RegionRegenerationLauncher extends BukkitRunnable {
         Entity[] entities = this.chunk.getEntities();
 
         for (Entity entity : entities) {
-            if (entity instanceof Player player && this.isPlayerWithinLaunchingGround(player)) {
-                this.region.getWorldType().playSound(player.getLocation(), Sound.BLOCK_AZALEA_LEAVES_STEP, 1, 1);
-                player.setVelocity(player.getLocation().getDirection().multiply(2).setX(0).setZ(0).setY(2));
+            if (entity instanceof LivingEntity livingEntity
+                    && this.isLivingEntityWithinLaunchingGround(livingEntity)
+            ) {
+                this.region.getWorldType().playSound(
+                        livingEntity.getLocation(), Sound.BLOCK_AZALEA_LEAVES_STEP, 1, 1
+                );
 
-                PotionEffect existingPotionEffect = player.getPotionEffect(PotionEffectType.SLOW_FALLING);
+                livingEntity.setVelocity(
+                        livingEntity.getLocation().getDirection().multiply(2).setX(0).setZ(0).setY(2)
+                );
+
+                PotionEffect existingPotionEffect = livingEntity.getPotionEffect(PotionEffectType.SLOW_FALLING);
                 int POTION_EFFECT_DURATION = 100;
 
                 if (existingPotionEffect != null) {
                     int existingDuration = existingPotionEffect.getDuration();
 
-                    player.addPotionEffect(new PotionEffect(
+                    livingEntity.addPotionEffect(new PotionEffect(
                             PotionEffectType.SLOW_FALLING,
                             existingDuration + POTION_EFFECT_DURATION, 0
                     ));
                 } else {
-                    player.addPotionEffect(new PotionEffect(
+                    livingEntity.addPotionEffect(new PotionEffect(
                             PotionEffectType.SLOW_FALLING, POTION_EFFECT_DURATION, 0
                     ));
                 }
