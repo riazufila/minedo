db5d6bf739a50a6eec61421161501aee868efbe3
riazufila <46749569+riazufila@users.noreply.github.com>
2023-09-27 23:14:41 +0800

    Update: Messages.

--- src/main/java/net/minedo/mc/constants/chattimeoutmessage/ChatTimeoutMessage.java
+++ src/main/java/net/minedo/mc/constants/chattimeoutmessage/ChatTimeoutMessage.java
@@ -3,7 +3,7 @@ package net.minedo.mc.constants.chattimeoutmessage;
 public enum ChatTimeoutMessage {
 
     INFO_CHAT_ENABLED("Chat enabled."),
-    ERROR_CHAT_TIMEOUT("You're sending too much message!");
+    ERROR_CHAT_TIMEOUT("Chat timeout.");
 
     private final String message;
 
--- src/main/java/net/minedo/mc/constants/likemessage/LikeMessage.java
+++ src/main/java/net/minedo/mc/constants/likemessage/LikeMessage.java
@@ -3,7 +3,7 @@ package net.minedo.mc.constants.likemessage;
 public enum LikeMessage {
 
     SUCCESS_LIKE_SENT("Sent %s a like."),
-    SUCCESS_LIKE_RECEIVED("%s sent you a like!"),
+    SUCCESS_LIKE_RECEIVED("%s liked you!"),
     ERROR_USAGE("Usage: /like <player>"),
     ERROR_REQUEST_PLAYER_IS_NOT_IN_SERVER("Player is not in the server."),
     ERROR_LIKE_SENT_RECENTLY("Only one like can be sent per day.");
