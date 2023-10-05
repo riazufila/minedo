bfa56b4261259cd43afdf72a9e56098393ff00ac
riazufila <46749569+riazufila@users.noreply.github.com>
2023-10-05 23:50:00 +0800

    Add: Immune to lightning.

--- src/main/java/net/minedo/mc/functionalities/customenchantment/list/LightningEnchantment.java
+++ src/main/java/net/minedo/mc/functionalities/customenchantment/list/LightningEnchantment.java
@@ -3,10 +3,13 @@ package net.minedo.mc.functionalities.customenchantment.list;
 import net.minedo.mc.constants.customenchantment.type.CustomEnchantmentType;
 import net.minedo.mc.functionalities.customenchantment.CustomEnchantment;
 import net.minedo.mc.functionalities.customenchantment.CustomEnchantmentUtils;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LightningStrike;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
@@ -32,7 +35,33 @@ public class LightningEnchantment extends CustomEnchantment implements Listener 
                     .isCustomEnchantmentInItem(itemAtHand, this.getCustomEnchantmentType());
 
             if (isCustomEnchantmentExist) {
-                player.getWorld().strikeLightning(event.getEntity().getLocation());
+                player.getWorld().strikeLightning(event.getEntity().getLocation()).setCausingPlayer(player);
+            }
+        }
+    }
+
+    @Override
+    @EventHandler
+    public void onDamaged(@NotNull EntityDamageEvent event) {
+        if (event.getCause() == EntityDamageEvent.DamageCause.LIGHTNING) {
+            EntityDamageByEntityEvent entityDamageByEntityEvent = (EntityDamageByEntityEvent) event;
+            Entity damager = entityDamageByEntityEvent.getDamager();
+
+            if (damager instanceof LightningStrike lightningStrike) {
+                if (lightningStrike.getCausingEntity() instanceof Player playerCausingLightningStrike) {
+                    ItemStack itemAtHand = playerCausingLightningStrike.getInventory().getItemInMainHand();
+
+                    if (itemAtHand.isEmpty()) {
+                        return;
+                    }
+
+                    boolean isCustomEnchantmentExist = CustomEnchantmentUtils
+                            .isCustomEnchantmentInItem(itemAtHand, this.getCustomEnchantmentType());
+
+                    if (isCustomEnchantmentExist) {
+                        event.setCancelled(true);
+                    }
+                }
             }
         }
     }
