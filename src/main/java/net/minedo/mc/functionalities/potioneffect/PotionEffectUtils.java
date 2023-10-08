5b1f3d2364ca17ad3fdd48a8942c274c0ab5a12b
riazufila <46749569+riazufila@users.noreply.github.com>
2023-10-08 13:29:29 +0800

    Add: Boilerplate for potion effects manager.

---
+++ src/main/java/net/minedo/mc/functionalities/potioneffect/PotionEffectUtils.java
@@ -0,0 +1,11 @@
+package net.minedo.mc.functionalities.potioneffect;
+
+public final class PotionEffectUtils {
+
+    public static int getTicksLeftBeforePotionEffectRunsOut() {
+        // TODO: Get time left before potion effect runs out.
+
+        return 0;
+    }
+
+}
