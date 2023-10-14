6f0d573c1bb91f26765ea716f9e85071ca6d7d28
riazufila <46749569+riazufila@users.noreply.github.com>
2023-10-14 22:26:09 +0800

    Add: Freeze effects on hit.

--- src/main/java/net/minedo/mc/functionalities/customenchantment/list/IceEnchantmentHandler.java
+++ src/main/java/net/minedo/mc/functionalities/customenchantment/list/IceEnchantmentHandler.java
@@ -1,8 +1,10 @@
 package net.minedo.mc.functionalities.customenchantment.list;
 
+import net.minedo.mc.constants.common.Common;
 import net.minedo.mc.constants.customenchantment.type.CustomEnchantmentType;
 import net.minedo.mc.constants.feedbacksound.FeedbackSound;
 import net.minedo.mc.customevents.PlayerNonBlockInteractEvent;
+import net.minedo.mc.functionalities.customenchantment.CombatEvent;
 import net.minedo.mc.functionalities.customenchantment.CustomEnchantment;
 import net.minedo.mc.functionalities.customenchantment.CustomEnchantmentHandler;
 import net.minedo.mc.functionalities.customenchantment.CustomEnchantmentWrapper;
@@ -11,9 +13,11 @@ import net.minedo.mc.functionalities.utils.ShapeUtils;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
@@ -32,6 +36,22 @@ public class IceEnchantmentHandler extends CustomEnchantmentHandler implements L
     public IceEnchantmentHandler(@NotNull HashMap<UUID, Integer> playerSkillPoints) {
         super(CustomEnchantmentType.ICE);
         this.playerSkillPoints = playerSkillPoints;
+    }
+
+    @EventHandler
+    public void onHit(@NotNull EntityDamageByEntityEvent event) {
+        CombatEvent combatEvent = super.isAbleToInflictCustomEnchantmentHits(event);
+
+        if (combatEvent == null || combatEvent.getCustomEnchantment() == null) {
+            return;
+        }
+
+        LivingEntity defendingEntity = combatEvent.getDefendingEntity();
+        int freezeTicks = defendingEntity.getFreezeTicks();
+        int updatedFreezeTicks = freezeTicks
+                + (combatEvent.getCustomEnchantment().getLevel() * (int) Common.TICK_PER_SECOND.getValue());
+
+        defendingEntity.setFreezeTicks(updatedFreezeTicks);
     }
 
     @EventHandler
