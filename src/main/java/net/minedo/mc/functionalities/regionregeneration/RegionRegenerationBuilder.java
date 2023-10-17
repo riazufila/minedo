65e42470f7c1703676427c52e04dbd0870069a7c
riazufila <46749569+riazufila@users.noreply.github.com>
2023-10-17 18:03:41 +0800

    Update: Region regeneration particles.

--- src/main/java/net/minedo/mc/functionalities/regionregeneration/RegionRegenerationBuilder.java
+++ src/main/java/net/minedo/mc/functionalities/regionregeneration/RegionRegenerationBuilder.java
@@ -145,10 +145,11 @@ public class RegionRegenerationBuilder extends BukkitRunnable {
                             x,
                             y,
                             z,
-                            10,
-                            1,
-                            1,
-                            1
+                            7,
+                            0.3,
+                            1.3,
+                            0.3,
+                            0
                     );
                 }
             }
